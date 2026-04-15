216f55c8ab8a3b5c368fd8a69eb8fea0d2dcc1bc
SumitRaj18 <sumitraj7162@gmail.com>
2026-04-15 12:04:11 +0530

    change

--- routes/jobRoute.js
+++ routes/jobRoute.js
@@ -7,7 +7,7 @@ const router =  express.Router()
 // POST | http://localhost:5000/api/v1/job/add
 router.route("/add").post(isAuthenticated,postJob)
 router.route("/all").get(getAllJobs);
-router.route("/explore/:id").get(getOneJob);
+router.route("/explore/:id").get(isAuthenticated,getOneJob);
 router.route("/adminjobs").get(isAuthenticated,getJobById)
 export default router
 
